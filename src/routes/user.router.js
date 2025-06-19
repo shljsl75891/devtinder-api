476855b509b3abe8537e01c901734a101f9cdc17
shljsl75891 <shljsl75891@gmail.com>
2025-06-19 18:22:29 +0530

    feat: password encryption using `bcrypt`

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import {Router} from 'express';
 import User from '../models/user.model.js';
 import {STATUS_CODES} from '../status-codes.js';
@@ -7,11 +8,19 @@ import {
 } from '../utils/validations/user.validation.js';
 
 const userRouter = Router();
+const SALT_ROUNDS = 10;
 
 userRouter.post('/signup', async (req, res) => {
   try {
     validateUserSignup(req.body);
-    const savedUser = await User.create(req.body);
+    const {firstName, lastName, email, password} = req.body;
+    const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
+    const savedUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
     res.status(STATUS_CODES.CREATED).json(savedUser);
   } catch (err) {
     res.status(STATUS_CODES.UNPROCESSABLE_ENTITY).send({message: err.message});
@@ -33,9 +42,13 @@ userRouter.get('/:id', async (req, res) => {
 userRouter.patch('/:id', async (req, res) => {
   try {
     validateUserUpdate(req.body);
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      runValidators: true,
-    });
+    const {profileImageUrl, password, skills} = req.body;
+    const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      {profileImageUrl, password: passwordHash, skills},
+      {runValidators: true},
+    );
     if (!user)
       res.status(STATUS_CODES.NOT_FOUND).json({message: 'User Not Found'});
     else res.sendStatus(STATUS_CODES.NO_CONTENT);
